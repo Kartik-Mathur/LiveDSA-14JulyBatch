9f833f3eefc11333fb255a6d08e6b71ffb1836e8
Kartik <kartik.mathur10@gmail.com>
2025-08-13 21:33:13 +0530

    Arrays-Sorting

---
+++ L11-Arrays/2_SelectionSort.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a[] = {5, 3, 1, 2, 4};
+	int n = sizeof(a) / sizeof(int);
+
+	for (int i = 0; i < n - 1; ++i)
+	{
+		int mi = i;
+		for (int j = i + 1; j < n; ++j)
+		{
+			if (a[j] < a[mi]) {
+				mi = j;
+			}
+		}
+
+		swap(a[mi], a[i]);
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		cout << a[i] << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
