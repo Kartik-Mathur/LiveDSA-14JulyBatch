987681f19cf4a87b50bada64368a8247c39c013d
Kartik <kartik.mathur10@gmail.com>
2025-10-10 22:25:50 +0530

    BinarySearch

---
+++ L29-QPS-Binary/KokoEatingBananas.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+	bool isPossible(vector<int>& piles, int h, int mid) {
+		int totalHourUsed = 0;
+		for (int i = 0; i < piles.size(); ++i)
+		{
+			totalHourUsed += (piles[i] / mid);
+			if (piles[i] % mid != 0) {
+				totalHourUsed++;
+			}
+		}
+
+		return totalHourUsed <= h;
+	}
+
+	int minEatingSpeed(vector<int>& piles, int h) {
+		int n = piles.size();
+		int s = 1;
+		int e = 0;
+
+		for (int i = 0; i < n; ++i)
+		{
+			e = max(e, piles[i]);
+		}
+
+		int ans;
+
+		while (s <= e) {
+			int mid = s + (e - s) / 2;
+
+			if (isPossible(piles, h, mid)) {
+				ans = mid;
+				e = mid - 1;
+			}
+			else {
+				s = mid + 1;
+			}
+		}
+
+		return ans;
+	}
+};
