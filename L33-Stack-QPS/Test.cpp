2db5cf3084c5020e37873f9cd729f50bb8633e56
Kartik <kartik.mathur10@gmail.com>
2025-10-27 22:57:02 +0530

    Stack QPS

---
+++ L33-Stack-QPS/Test.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+
+	for (int i = 0; i < n; ++i)
+	{
+
+		if (a[i] < 10) {
+
+		}
+
+		// Here we always would have a[i] >= 10
+	}
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
