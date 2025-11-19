fa846ec614784ad02063aaaf67cf6440e18de723
Kartik <kartik.mathur10@gmail.com>
2025-11-19 22:18:43 +0530

    Recursion-QPS

--- L41-RecursionAndTreesQPS.cpp/2_CombinationSum2.cpp
+++ L41-RecursionAndTreesQPS.cpp/2_CombinationSum2.cpp
@@ -4,28 +4,53 @@ public:
 
 	void solve(vector<int> &ip, vector<int> &op, int i, int target) {
 		if (target == 0) {
-			ans.push_back(op); // op wale vector ko store krlo kyunki uske andar ek answer hoga...
+			ans.push_back(op);
 			return;
 		}
-		// base case
+
 		if (i == ip.size()) {
 			return;
 		}
 
 		for (int j = i; j < ip.size(); ++j)
 		{
-			if (target >= ip[j]) {
+			// i and j are not equal aur j apne pehle wale index ke barabar hai toh isse nhi lena
+			if (i != j and ip[j] == ip[j - 1]) continue;
+
+			if (ip[j] <= target) {
 				op.push_back(ip[j]);
-				solve(ip, op, j, target - ip[j]);
+				solve(ip, op, j + 1, target - ip[j]);
 				op.pop_back();
 			}
 		}
 	}
 
-	vector<vector<int>> combinationSum(vector<int>& ip, int target) {
+	vector<vector<int> > combinationSum2(vector<int>& ip, int target) {
 		vector<int> op;
+		sort(ip.begin(), ip.end());
 		solve(ip, op, 0, target);
 
 		return ans;
 	}
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
